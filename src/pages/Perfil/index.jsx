722ba9af7a4e753d7cf9130518f5500419b68ba7
DenisBuarqueDev <denisbuarque@gmail.com>
2025-08-18 10:56:34 -0300

    Atualizando dados usuário no fontend

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuthValue } from "../../context/AuthContextProvider";
 import Profile from "../../components/UserProfile/Profile";
-import AddressProfile from "../../components/UserProfile/AddressProfile";
+//import AddressProfile from "../../components/UserProfile/AddressProfile";
 
 const index = () => {
   const { user } = useAuthValue();
@@ -10,7 +10,7 @@ const index = () => {
     <main className="flex flex-col w-full p-4 md:py-4">
       <section className="max-w-screen-md w-full flex flex-col mx-auto">
         {user && <Profile id={user.id} />}
-        {user && <AddressProfile id={user.id} />}
+        {/*user && <AddressProfile id={user.id} /> */}
       </section>
     </main>
   );
