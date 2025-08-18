7d981efd98869db2b071b6924b8482d039cdfafd
DenisBuarqueDev <denisbuarque@gmail.com>
2025-08-18 11:15:47 -0300

    Ajustando dados do endereço do usuário

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -1,16 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthValue } from "../../context/AuthContextProvider";
-import Profile from "../../components/UserProfile/Profile";
 //import AddressProfile from "../../components/UserProfile/AddressProfile";
+import { Link } from "react-router-dom";
+import api from "../../api/axios";
+import { MdAddAPhoto, MdNoPhotography } from "react-icons/md";
+import { FaEdit, FaMapMarkerAlt, FaPlus, FaUserCircle } from "react-icons/fa";
 
 const index = () => {
   const { user } = useAuthValue();
+  const [data, setData] = useState(null);
+  const [addresses, setAddresses] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  // Função busca os dados do usuário
+  const fetchUser = async () => {
+    try {
+      setLoading(true);
+      const res = await api.get(`api/auth/${user.id}`, {
+        withCredentials: true,
+      });
+      setData(res.data);
+    } catch (error) {
+      console.error("Erro ao buscar dados do usuário:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Função para buscar os endereços
+  const fetchAddresses = async () => {
+    try {
+      setLoading(true);
+      const res = await api.get(`api/addresses/user/${user.id}`);
+      setAddresses(res.data.data || []);
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.id) {
+      fetchUser();
+      fetchAddresses();
+    }
+  }, [user.id]);
+
+  if (loading) {
+    return (
+      <div role="status" className="max-w-md w-full animate-pulse">
+        <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[330px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[300px] mb-2.5"></div>
+        <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 max-w-[360px]"></div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  }
 
   return (
     <main className="flex flex-col w-full p-4 md:py-4">
       <section className="max-w-screen-md w-full flex flex-col mx-auto">
-        {user && <Profile id={user.id} />}
-        {/*user && <AddressProfile id={user.id} /> */}
+        {user && data && (
+          <>
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex flex-col items-end">
+                {data.image ? (
+                  <img
+                    className="w-32 h-32 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                    src={`${import.meta.env.VITE_API_URL}${data.image}`}
+                    alt="Foto"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center w-32 h-32 text-white text-sm rounded-full bg-gray-400">
+                    <MdNoPhotography />
+                  </div>
+                )}
+                <Link to={`/photo/${data._id}`} className="p-2">
+                  <MdAddAPhoto className="w-4 h-4" />
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col flex-1 py-4">
+              <h2 className="flex items-center gap-2 text-md font-medium pb-2 border-b">
+                <FaUserCircle />
+                Perfil
+              </h2>
+              <p className="text-sm">Nome: {data.firstName}</p>
+              <p className="text-sm">Sobrenome: {data.lastName}</p>
+              <p className="text-sm">Telefone: {data.phone}</p>
+              <p className="text-sm">E-mail: {data.email}</p>
+            </div>
+          </>
+        )}
+      </section>
+      <section className="max-w-screen-md w-full flex flex-col mx-auto">
+        <div className="flex items-center justify-between border-b py-2 mb-1">
+          <h2 className="flex items-center text-md font-medium">
+            <FaMapMarkerAlt className="mr-2" />
+            Endereço
+          </h2>
+          {addresses && addresses.length === 0 && (
+            <Link to="/address/create">
+              <FaPlus />
+            </Link>
+          )}
+        </div>
+
+        {addresses && addresses.length === 0 ? (
+          <div
+            className="p-4 text-sm border border-red-300 text-red-800 rounded-lg bg-red-50 mt-2 dark:bg-gray-800 dark:text-red-400"
+            role="alert"
+          >
+            {error}
+          </div>
+        ) : (
+          <ul>
+            {addresses.map((address, index) => (
+              <li
+                key={index}
+                className="flex items-center justify-between text-sm"
+              >
+                <p>
+                  {address.street}, {address.number}, {address.district},{" "}
+                  {address.zipCode}, {address.city} - {address.state},{" "}
+                  {address.complement}
+                </p>
+                <Link to={`/address/edit/${address._id}`}>
+                  <FaEdit />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </section>
     </main>
   );
