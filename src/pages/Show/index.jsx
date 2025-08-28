c2d96dd4132bed3ae144526c418bbb48c665f08a
DenisBuarqueDev <denisbuarque@gmail.com>
2025-08-28 10:53:03 -0300

    Ajustanto margin

--- src/pages/Show/index.jsx
+++ src/pages/Show/index.jsx
@@ -83,7 +83,7 @@ const index = () => {
     return (
       <div
         role="status"
-        className="max-w-sm m-auto p-4 m-3 border border-gray-200 rounded-sm shadow-sm animate-pulse md:p-6 dark:border-gray-700"
+        className="max-w-sm m-auto p-4 border border-gray-200 rounded-sm shadow-sm animate-pulse md:p-6 dark:border-gray-700"
       >
         <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded-sm dark:bg-gray-700">
           <svg
